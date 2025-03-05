fd4c193f0795aaf94b2fa84178bc11765c2546bb
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-05 18:57:42 +0100

    Update main_01.py

---
+++ main_01.py
@@ -0,0 +1,11 @@
+'''
+Zadatak 1.
+Definirajte klasu Student s atributima:
+    ime, prezime, broj indeksa i godina studija.
+
+Kreirajte konstruktor za inicijalizaciju ovih atributa.
+Dodajte metodu prikazi_podatke koja ispisuje sve podatke o studentu.
+'''
+
+
+ime, prezime, broj indeksa i godina studija.

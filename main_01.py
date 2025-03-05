72d05a128ae4ca183fed494780168532b8778f9e
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-05 19:19:44 +0100

    Update main_01.py

--- main_01.py
+++ main_01.py
@@ -12,6 +12,7 @@ ime = 'Pero'
 prezime = 'Peric'
 broj_indeksa = '123456'
 godina_studija = 2021
+prosjek_ocjena = 4.89
 
 # Ispis podataka
 print('Ispis iz varijabli')
@@ -19,6 +20,7 @@ print(ime)
 print(prezime)
 print(broj_indeksa)
 print(godina_studija)
+print(prosjek_ocjena)
 print()
 
 
@@ -29,7 +31,8 @@ student = {
     'ime': 'Pero',
     'prezime': 'Peric',
     'broj_indeksa': '123456',
-    'godina_studija': 2021
+    'godina_studija': 2021,
+    'prosjek_ocjena': 4.89
 }
 
 # Ispis podataka
@@ -38,6 +41,7 @@ print(student['ime'])
 print(student['prezime'])
 print(student['broj_indeksa'])
 print(student['godina_studija'])
+print(student['prosjek_ocjena'])
 print()
 
 
@@ -54,14 +58,27 @@ class Student:
     prezime = 'Peric'
     broj_indeksa = '123456'
     godina_studija = 2021
+    prosjek_ocjena = 4.89
 
 
 pero = Student()
 
 # Ispis podataka
-print('Ispis iz klase Student')
+print('Ispis iz klase Student - Pero')
 print(pero.ime)
 print(pero.prezime)
 print(pero.broj_indeksa)
 print(pero.godina_studija)
+print(pero.prosjek_ocjena)
+print()
+
+
+ana = Student()
+# Ispis podataka
+print('Ispis iz klase Student - Ana')
+print(ana.ime)
+print(ana.prezime)
+print(ana.broj_indeksa)
+print(ana.godina_studija)
+print(ana.prosjek_ocjena)
 print()

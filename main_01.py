af5821602d65aa8daffa683cc3c78a52909ec5a7
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-05 19:55:53 +0100

    Update main_01.py

--- main_01.py
+++ main_01.py
@@ -53,7 +53,7 @@ def create_student(ime, prezime, broj_indeksa, godina_studija, prosjek_ocjena):
     interna_var_godina_studija = godina_studija
     interna_var_prosjek_ocjena = prosjek_ocjena
 
-
+create_student('Pero', 'Peric', '123456', 2021, 4.89)
 
 
 
@@ -67,30 +67,48 @@ class Student:
         self.godina_studija = godina_studija
         self.prosjek_ocjena = prosjek_ocjena
 
+    def ispis(self):
+        print(f'Ispis iz metode u klasi Student - {self.ime}')
+        print(f'Ime: {self.ime}')
+        print(f'Prezime: {self.prezime}')
+        print(f'Broj indeksa: {self.broj_indeksa}')
+        print(f'Godina studija {self.godina_studija}')
+        print(f'Prosjek ocjena {self.prosjek_ocjena}')
+        print()
 
-pero = Student()
 
+
+
+
+
+
+
+
+
+pero = Student('Pero', 'Peric', '123456', 2021, 4.89)
 # Ispis podataka
-print('Ispis iz klase Student - Pero')
-print(pero.ime)
-print(pero.prezime)
-print(pero.broj_indeksa)
-print(pero.godina_studija)
-print(pero.prosjek_ocjena)
-print()
+# print('Ispis iz klase Student - Pero')
+# print(pero.ime)
+# print(pero.prezime)
+# print(pero.broj_indeksa)
+# print(pero.godina_studija)
+# print(pero.prosjek_ocjena)
+# print()
+pero.ispis()
 
 
-ana = Student()
-ana.ime = 'Ana'
-ana.prezime = 'Anic'
-ana.broj_indeksa = '654321'
-ana.godina_studija = 2024
-ana.prosjek_ocjena = 4.98
+ana = Student('Ana', 'Anic', '654321', 2024, 4.98)
+# ana.ime = 'Ana'
+# ana.prezime = 'Anic'
+# ana.broj_indeksa = '654321'
+# ana.godina_studija = 2024
+# ana.prosjek_ocjena = 4.98
 # Ispis podataka
-print('Ispis iz klase Student - Ana')
-print(ana.ime)
-print(ana.prezime)
-print(ana.broj_indeksa)
-print(ana.godina_studija)
-print(ana.prosjek_ocjena)
-print()
+# print('Ispis iz klase Student - Ana')
+# print(ana.ime)
+# print(ana.prezime)
+# print(ana.broj_indeksa)
+# print(ana.godina_studija)
+# print(ana.prosjek_ocjena)
+# print()
+ana.ispis()

8044c42e69ddbd8bd704ed1c451051f8e99ef57b
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-05 19:13:22 +0100

    Update main_01.py

--- main_01.py
+++ main_01.py
@@ -8,4 +8,60 @@ Dodajte metodu prikazi_podatke koja ispisuje sve podatke o studentu.
 '''
 
 
-ime, prezime, broj indeksa i godina studija.
+ime = 'Pero'
+prezime = 'Peric'
+broj_indeksa = '123456'
+godina_studija = 2021
+
+# Ispis podataka
+print('Ispis iz varijabli')
+print(ime)
+print(prezime)
+print(broj_indeksa)
+print(godina_studija)
+print()
+
+
+
+
+
+student = {
+    'ime': 'Pero',
+    'prezime': 'Peric',
+    'broj_indeksa': '123456',
+    'godina_studija': 2021
+}
+
+# Ispis podataka
+print('Ispis iz rjecnika')
+print(student['ime'])
+print(student['prezime'])
+print(student['broj_indeksa'])
+print(student['godina_studija'])
+print()
+
+
+
+
+
+
+
+
+
+
+class Student:
+    ime = 'Pero'
+    prezime = 'Peric'
+    broj_indeksa = '123456'
+    godina_studija = 2021
+
+
+pero = Student()
+
+# Ispis podataka
+print('Ispis iz klase Student')
+print(pero.ime)
+print(pero.prezime)
+print(pero.broj_indeksa)
+print(pero.godina_studija)
+print()
